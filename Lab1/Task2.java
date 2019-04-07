9333a631659993c309ae97708e50835156f4d3d3
umarsoomro <49340982+umarsoomro@users.noreply.github.com>
2019-04-07 14:10:37 -0700

    Add files via upload

---
+++ Lab1/Task2.java
@@ -0,0 +1,13 @@
+class Task2{
+public static void main(String[]args){
+
+   double Value=50.25;
+   int Integer=(int)Value;
+   double Fraction=Value-Integer;
+	
+	System.out.println("Value = "+ Value);
+    System.out.println("Integer = "+ Integer);
+    System.out.println("Fraction = "+ Fraction);
+
+}
+}

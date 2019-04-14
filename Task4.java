688a183b5b4c86df4b3ba29dc72f72ec81372bdb
umarsoomro <49340982+umarsoomro@users.noreply.github.com>
2019-04-14 12:04:53 -0700

    Add files via upload

---
+++ Task4.java
@@ -0,0 +1,14 @@
+class Task4{
+public static void main(String args[]){
+	int rad = 4;
+	double dia = 2*rad;
+	double circum = 2*3.14*rad;
+	double area = 3.14*rad*rad;
+	System.out.println("Radius: "+rad);
+	System.out.println("Diameter: "+dia);
+	System.out.println("Circumference: "+circum);
+	System.out.println("Area: "+area);
+	
+	
+}
+}
